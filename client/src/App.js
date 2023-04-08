7b0f50b565f7c490966002bce232d5b691fdc3f8
Gaurav <garo7740@colorado.edu>
2023-04-08 17:46:06 -0600

    Added React Bootstrap

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,10 @@
 import './App.css';
 import Header from "./components/Header/Header";
 import List from "./components/List/List";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Card from 'react-bootstrap/Card';
 
 function App() {
 
@@ -8,12 +12,38 @@ function App() {
         type: 'Movies', list: ["The Godfather", "Fight Club", "3 Idiots"]
     }, {type: 'Movies', list: ["The Godfather", "Fight Club", "3 Idiots"]}];
 
+    const movies = [
+        {
+        "name" : "The Godfather",
+        "genre": "Action, Thriller",
+          "img" : "https://media.npr.org/assets/img/2022/03/10/marlon-brandon-the-godfather_custom-e18469fdceae838fc9057f1f9ab1ddb019a66670-s1100-c50.jpg",
+          "duration" : "126",
+          "summary" : "Widely regarded as one of the greatest films of all time, this mob drama, based on Mario Puzo's novel of the same name, focuses on the powerful Italian-American crime family of Don Vito Corleone (Marlon Brando). ",
+          "Ratings" : "5"
+        }
+      ];
+
     return (<div>
         <Header/>
         <div className='body'>
-            {arr.map((a, i) => <List type={a.type} list={a.list}/>)}
+            {arr.map((a, i) => <List type={a.type} list={a.list}/>)} 
+            {/* <List movies= {movies} name = "Movies"/> */}
+
+            <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src="holder.js/100px180" />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
         </div>
     </div>);
 }
 
 export default App;
+
+
--- client/src/components/List/List.js
+++ client/src/components/List/List.js
@@ -1,14 +1,34 @@
 import React, {useState} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Container from 'react-bootstrap/Row';
+import Button from 'react-bootstrap/Button';
 
 function List(props) {
+    
     return (
-        <div>
-            <h3>{props.type}</h3>
-            {props.list.map((li, i) => {
-                return <div key={i}>{li}</div>
-            })}
-        </div>
+        // <div>
+        //     <h3>{props.type}</h3>
+        //     <h3>{props.img}</h3>
+        //     {props.list.map((li, i) => {
+        //         return <div key={i}>{li}</div>
+        //     })}
+        // </div>
+
+        <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src="holder.js/100px180" />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
+
     );
 }
-
 export default List;
