b7e625d25ec77cb377bd7863249a71b30ee49885
Gaurav <garo7740@colorado.edu>
2023-04-08 17:50:53 -0600

    Added template for Cards

--- client/src/App.js
+++ client/src/App.js
@@ -26,20 +26,10 @@ function App() {
     return (<div>
         <Header/>
         <div className='body'>
-            {arr.map((a, i) => <List type={a.type} list={a.list}/>)} 
-            {/* <List movies= {movies} name = "Movies"/> */}
-
-            <Card style={{ width: '18rem' }}>
-        <Card.Img variant="top" src="holder.js/100px180" />
-        <Card.Body>
-          <Card.Title>Card Title</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
-          <Button variant="primary">Go somewhere</Button>
-        </Card.Body>
-      </Card>
+            {/* {arr.map((a, i) => <List type={a.type} list={a.list}/>)}  */}
+           
+            <List movies= {movies} name = "Movies"/>
+        
         </div>
     </div>);
 }
--- client/src/components/List/List.js
+++ client/src/components/List/List.js
@@ -7,7 +7,10 @@ import Container from 'react-bootstrap/Row';
 import Button from 'react-bootstrap/Button';
 
 function List(props) {
+    console.log(props);
+    console.log(props.movies[0]);
     
+    //console.log(arr[0].name);
     return (
         // <div>
         //     <h3>{props.type}</h3>
@@ -17,17 +20,39 @@ function List(props) {
         //     })}
         // </div>
 
-        <Card style={{ width: '18rem' }}>
-        <Card.Img variant="top" src="holder.js/100px180" />
-        <Card.Body>
-          <Card.Title>Card Title</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
-          <Button variant="primary">Go somewhere</Button>
-        </Card.Body>
-      </Card>
+        <div>
+       
+ <Container >
+ <h3>{props.type}</h3>
+ <h1>{props.name}</h1>
+    <Row xs={1} md={4} className="g-4">
+           
+      {Array.from({ length: 8 }).map((_, idx) => (
+        <Col>
+          <Card>
+          <Card.Img style={{ width: 'auto', height : '200px'}} variant="top" src= {props.movies[0].img} />
+          <Card.Body>
+            <Card.Title>{props.movies[0].name}</Card.Title>
+            <Card.Text>
+                {/* {props.movies[0].summary} */}
+             
+            </Card.Text>
+            <p1 style={{ color: 'red' }} >Genre :   </p1>
+            <p1>{props.movies[0].genre} </p1> 
+            <br></br>
+            <p1 style={{ color: 'red' }} > Ratings: </p1> 5/5
+            <br></br>
+            <p1 style={{ color: 'red' }} > Length: </p1> {props.movies[0].duration}
+            <hr></hr>
+            <Button variant="danger">More Details</Button>
+          </Card.Body>
+        </Card>
+      </Col>
+    ))}
+  </Row>
+ 
+  </Container>
+        </div>
 
     );
 }
